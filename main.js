bee1eaba1aa7de92ae981ccadf36df2215bd7686
Sahil0071 <sahilraja242@gmail.com>
2018-03-12 12:49:55 +0530

    Final commit

---
+++ main.js
@@ -0,0 +1,14 @@
+alert("Connected");
+var btn = document.querySelector("button");
+//var isPurple = false;
+/*btn.addEventListener("click", function() {
+    if (isPurple) {
+        document.body.style.background = "white";
+    } else {
+        document.body.style.background = "purple";
+    }
+    isPurple = !isPurple;
+});*/
+btn.addEventListener("click", function() {
+    document.body.classList.toggle("purple");
+})
